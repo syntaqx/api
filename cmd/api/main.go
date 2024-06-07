ac1dfbf5dbd559311f2cd20e3d5c29668d857d6c
Chase Pierce <syntaqx@gmail.com>
2024-06-07 13:33:20 -0600

    fileserver handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -36,7 +35,7 @@ func main() {
 
 	workDir, _ := os.Getwd()
 	filesDir := http.Dir(filepath.Join(workDir, "static"))
-	FileServer(r, "/", filesDir)
+	handler.FileServer(r, "/", filesDir)
 
 	srv := &http.Server{
 		Addr:    net.JoinHostPort("", port),
@@ -49,25 +48,4 @@ func main() {
 			fmt.Printf("http server closed unexpectedly: %v\n", err)
 		}
 	}
-}
-
-// FileServer conveniently sets up a http.FileServer handler to serve
-// static files from a http.FileSystem.
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
 }
