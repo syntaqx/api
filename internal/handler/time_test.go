92508e94f544cc11f979a7f5baf885ff2f897474
Chase Pierce <syntaqx@gmail.com>
2024-06-07 19:13:53 -0600

    add in test for coverage results

---
+++ internal/handler/time_test.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimeHandler_CurrentTime(t *testing.T) {
+	handler := NewTimeHandler()
+
+	req, err := http.NewRequest("GET", "/time", nil)
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	router := chi.NewRouter()
+	router.Get("/time", handler.CurrentTime)
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
+
+	var response TimeResponse
+	err = json.Unmarshal(rr.Body.Bytes(), &response)
+	assert.NoError(t, err)
+
+	expectedTime := time.Now().UTC().Format(time.RFC3339)
+	assert.Equal(t, expectedTime, response.CurrentTime)
+}
