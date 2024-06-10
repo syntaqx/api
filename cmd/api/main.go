78af6b3ccb30824e05ee5646b79d19eb62884fcc
Chase Pierce <syntaqx@gmail.com>
2024-06-10 13:34:05 -0600

    update usage of zap

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,11 +9,19 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/syntaqx/zapchi"
+	"go.uber.org/zap"
 
 	"github.com/syntaqx/api/internal/handler"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(fmt.Errorf("unable to initialize logger: %s", err))
+	}
+	defer logger.Sync()
+
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = "8080"
@@ -25,14 +33,17 @@ func main() {
 
 	r := chi.NewRouter()
 
+	// Base middleware
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
+	r.Use(zapchi.Logger(logger, "router"))
 	r.Use(middleware.Recoverer)
 
+	// Register routes
 	rootHandler.RegisterRoutes(r)
 	timeHandler.RegisterRoutes(r)
 
+	// Static files
 	workDir, _ := os.Getwd()
 	filesDir := http.Dir(filepath.Join(workDir, "static"))
 	handler.FileServer(r, "/", filesDir)
@@ -42,10 +53,10 @@ func main() {
 		Handler: r,
 	}
 
-	fmt.Printf("http listenning on port %s\n", port)
+	logger.Info("http server started", zap.String("addr", srv.Addr))
 	if err := srv.ListenAndServe(); err != nil {
 		if err != http.ErrServerClosed {
-			fmt.Printf("http server closed unexpectedly: %v\n", err)
+			logger.Error("http server closed unexpectedly", zap.Error(err))
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.22.3
 require (
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/stretchr/testify v1.9.0
+	github.com/syntaqx/zapchi v0.0.0-20240610193050-79db48c38f2d
+	go.uber.org/zap v1.27.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
