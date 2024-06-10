1f8567e40837ba35379ff561d9144a08273a48b7
Chase Pierce <syntaqx@gmail.com>
2024-06-10 13:43:25 -0600

    test all but main

---
+++ internal/handler/fileserver_test.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFileServer_FileNotFound(t *testing.T) {
+	r := chi.NewRouter()
+	root := http.Dir(".")
+
+	// Call the FileServer function
+	FileServer(r, "/static", root)
+
+	// Create a new request to the static file URL
+	req, err := http.NewRequest("GET", "/static/file-not-found", nil)
+	assert.NoError(t, err)
+
+	// Create a new response recorder
+	rr := httptest.NewRecorder()
+
+	// Serve the request using the router
+	r.ServeHTTP(rr, req)
+
+	// Check the response status code
+	assert.Equal(t, http.StatusNotFound, rr.Code)
+}
